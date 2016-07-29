1dbd316992ed04cea540556b76c99fff8e74c0de
ESTRELLA <ESTRELLA@LAPTOP-C8LIA1GE.mshome.net>
2016-07-29 17:01:59 -0500

    Carga de scripts

---
+++ AppDemoGas/public_html/js/login.js
@@ -0,0 +1,16 @@
+  $(document).ready(function () {
+        //alert('is jquery loaded');
+    });
+    
+function validate() {
+    var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+    if (username == "Admin" && password == "admin123") {
+        alert("Login successfully");
+        window.location = "success.html"; // Redirecting to other page.
+        return false;
+    }
+    else {
+        alert("El Nombre/contraseña son incorrectos");
+    }
+}
